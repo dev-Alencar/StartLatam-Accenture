efdb647a37531a895932c447f1a2db5b4f2fafbc
Luc Alencar <lucalencar.dev@gmail.com>
2020-08-06 01:36:25 -0300

    oneCommit

---
+++ Canvas/Atv enviadas/JavaScript/JS  05-08-20/script.js
@@ -0,0 +1,4 @@
+const number1 = prompt("Escreva a primeira frase")
+const number2 = prompt("Escreva a segunda frase")
+const number3 = prompt("Escreva a última frase")
+const junto = document.querySelector(".box").innerHTML = `${number1} e ${number2}, então ${number3}`

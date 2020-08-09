08f3f0f5325aca90a71e0c72f44b6dcc26967854
Luc Alencar <lucalencar.dev@gmail.com>
2020-08-08 22:30:58 -0300

    commit chat

---
+++ Canvas/Atv enviadas/JavaScript/JS - chat/script.js
@@ -0,0 +1,28 @@
+
+function primeiroBotao(){
+    var variavel1 = document.querySelector(".primeira").value
+
+    var newVar1 = document.createElement("p");
+    newVar1.innerHTML =`A: ${variavel1}`;
+    document.querySelector(".chat").appendChild(newVar1);
+
+    if(variavel1 === "socorro" || variavel1 ==="SOCORRO"){
+        alert("BOMBEIROS")
+    } 
+    
+}
+
+
+
+
+function segundoBotao(){
+    var variavel2 = document.querySelector(".segunda").value
+
+    var newVar2 = document.createElement("p");
+    newVar2.innerHTML = `B: ${variavel2}`
+    document.querySelector(".chat").appendChild(newVar2);
+
+    if(variavel2 === "socorro" || variavel2 === "SOCORRO"){
+        alert("BOMBEIROS")
+    }
+}

527a1d7ab6b548d2f9a77ba0d7a59dfd90b87dbd
Luc Alencar <lucalencar.dev@gmail.com>
2020-08-09 12:15:31 -0300

    second commit chat

--- Canvas/Atv enviadas/JavaScript/JS - chat/script.js
+++ Canvas/Atv enviadas/JavaScript/JS - chat/script.js
@@ -1,10 +1,10 @@
 
 function primeiroBotao(){
     var variavel1 = document.querySelector(".primeira").value
-
-    var newVar1 = document.createElement("p");
+    var newVar1 = document.createElement("h6");
     newVar1.innerHTML =`A: ${variavel1}`;
-    document.querySelector(".chat").appendChild(newVar1);
+    document.querySelector(".chat").appendChild (newVar1);
+
 
     if(variavel1 === "socorro" || variavel1 ==="SOCORRO"){
         alert("BOMBEIROS")
@@ -15,12 +15,13 @@ function primeiroBotao(){
 
 
 
+
 function segundoBotao(){
     var variavel2 = document.querySelector(".segunda").value
-
     var newVar2 = document.createElement("p");
     newVar2.innerHTML = `B: ${variavel2}`
     document.querySelector(".chat").appendChild(newVar2);
+
 
     if(variavel2 === "socorro" || variavel2 === "SOCORRO"){
         alert("BOMBEIROS")

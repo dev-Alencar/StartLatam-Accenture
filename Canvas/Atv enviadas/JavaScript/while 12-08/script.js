6427fe6f7f8f1e142d8df7332eb1935343e5b3cd
Luc Alencar <lucalencar.dev@gmail.com>
2020-08-12 21:02:54 -0300

    while

---
+++ Canvas/Atv enviadas/JavaScript/while 12-08/script.js
@@ -0,0 +1,11 @@
+var num1 = Number(prompt(`Digite o primeiro valor`))
+var num2 = Number(prompt(`Digite o segundo valor`))
+var i = 0
+
+
+while(i < num2 ){   
+var result = num1 +num2
+alert(`resultado: ${result}`)
+
+i++
+}

e6aa8defa6a576fa46c4241c685fc5d432c184bc
Luc Alencar <lucalencar.dev@gmail.com>
2020-08-19 23:56:37 -0300

    secondMultiplos

---
+++ Canvas/Atv enviadas/JavaScript/Múltiplos/script.js
@@ -0,0 +1,48 @@
+function bootclicar(){
+    var variavel1 = document.querySelector(".inicia").value
+    var newVar = document.createElement("h1");
+    
+
+    if (variavel1% 3 === 0 && variavel1% 5 === 0 && variavel1 != 0 ){
+
+        newVar. innerHTML = `BORA DALE <br>`
+        document.querySelector(".container").appendChild(newVar)
+
+
+        newVar.style.color = "#6959CD"
+
+    } else if (variavel1%3 === 0  && variavel1 != 0){
+        newVar. innerHTML = `BORA<br>`
+
+      document.querySelector(".container").appendChild(newVar)
+
+       newVar.style.color = "#00FA9A"
+
+
+    }else if (variavel1%5 === 0 && variavel1 != 0 ){
+        newVar. innerHTML = `DALE <br>`
+       document.querySelector(".container").appendChild(newVar)
+
+        newVar.style.color = "#FF1493"
+
+    } else  {
+        newVar. innerHTML = `${variavel1} <br>`
+        document.querySelector(".container").appendChild(newVar)
+
+    }
+
+
+}
+
+
+
+
+function limpa(){
+   let segundo  = document.querySelector(".container")
+   segundo.innerHTML = " "
+}
+
+
+
+
+
